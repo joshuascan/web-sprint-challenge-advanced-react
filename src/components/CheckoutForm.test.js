13e9fc02e2287503486dbc592c3763ba8b32d5b8
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-06-04 11:41:33 -0700

    Builds the 2 CheckoutForm tests

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,42 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />);
+  const header = screen.queryByText(/checkout form/i);
 
-test("form shows success message on submit with form details", () => {});
+  expect(header).toBeInTheDocument();
+  expect(header).toHaveTextContent(/checkout form/i);
+});
+
+test("form shows success message on submit with form details", () => {
+  render(<CheckoutForm />);
+  const firstNameInput = screen.getByLabelText(/first name/i);
+  const lastNameInput = screen.getByLabelText(/last name/i);
+  const addressInput = screen.getByLabelText(/address/i);
+  const cityInput = screen.getByLabelText(/city/i);
+  const stateInput = screen.getByLabelText(/state/i);
+  const zipInput = screen.getByLabelText(/zip/i);
+  const checkoutButton = screen.getByRole("button");
+
+  userEvent.type(firstNameInput, "Josh");
+  userEvent.type(lastNameInput, "Scanlan");
+  userEvent.type(addressInput, "123 Test Street");
+  userEvent.type(cityInput, "Faketown");
+  userEvent.type(stateInput, "CA");
+  userEvent.type(zipInput, "54321");
+  userEvent.click(checkoutButton);
+
+  const successMessage = screen.getByTestId(/successmessage/i);
+  expect(successMessage).toBeInTheDocument();
+  expect(successMessage).toHaveTextContent(firstNameInput.value);
+  expect(successMessage).toHaveTextContent(lastNameInput.value);
+  expect(successMessage).toHaveTextContent(addressInput.value);
+  expect(successMessage).toHaveTextContent(cityInput.value);
+  expect(successMessage).toHaveTextContent(stateInput.value);
+  expect(successMessage).toHaveTextContent(zipInput.value);
+});
